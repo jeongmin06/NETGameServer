0d7e7e4d99569faa3ec13bb3120c8646b84e8c2e
jeongmin06 <jongmin06@naver.com>
2025-11-23 21:39:33 +0900

    Add ActorThreadPool

---
+++ Server.Core/ActorThreadPool.cs
@@ -0,0 +1,34 @@
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Server.Core;
+
+public sealed class ActorThreadPool
+{
+    private readonly Channel<ActorThread> _actorThreadPool;
+    private readonly Channel<ActorChannel<IActorMessage>> _readyChannelQueue;
+
+    public ActorThreadPool(int threadCount)
+    {
+        _actorThreadPool = Channel.CreateBounded<ActorThread>(threadCount);
+        _readyChannelQueue = Channel.CreateUnbounded<ActorChannel<IActorMessage>>(new UnboundedChannelOptions
+        {
+            SingleReader = false,
+            SingleWriter = false
+        });
+    }
+
+    public async Task AddReadyChannel(ActorChannel<IActorMessage> actorChannel)
+    {
+        await _readyChannelQueue.Writer.WriteAsync(actorChannel);
+    }
+
+    public async Task RunProcessing()
+    {
+        await foreach (var channel in _readyChannelQueue.Reader.ReadAllAsync())
+        {
+            var actorThread = await _actorThreadPool.Reader.ReadAsync();
+            await actorThread.RunAsync(channel);
+            await _actorThreadPool.Writer.WriteAsync(actorThread);
+        }
+    }
+}
