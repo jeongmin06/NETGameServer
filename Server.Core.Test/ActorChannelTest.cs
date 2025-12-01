a3d2f7c752250d6fa50ec91411a82db6b934a525
jeongmin06 <jongmin06@naver.com>
2025-12-01 22:38:45 +0900

    Add Server.Core.Test Project & ActorChannelTest

---
+++ Server.Core.Test/ActorChannelTest.cs
@@ -0,0 +1,44 @@
+﻿namespace Server.Core.Test;
+
+public class ActorChannelTest
+{
+    [Fact]
+    public async Task Messages_Are_Processed_In_Order()
+    {
+        using var cts = new CancellationTokenSource();
+        var channel = new ActorChannel(cts.Token);
+
+        var result = new List<int>();
+        var lockObj = new object();
+
+        for (int i = 0; i < 100; i++)
+        {
+            int capture = i;
+            channel.Post(new TestMessage(async () =>
+            {
+                lock (lockObj)
+                {
+                    result.Add(capture);
+                }
+                await Task.CompletedTask; 
+            }));
+        }
+
+        _ = ActorThreadPool.Instance;
+
+        await Task.Delay(500);
+
+        for (int i = 0; i < 100; i++)
+        {
+            Assert.Equal(i, result[i]);
+        }
+    }
+
+    private sealed class TestMessage(Func<ValueTask> action) : IActorMessage
+    {
+        public async ValueTask RunAsync()
+        {
+            await action();
+        }
+    }
+}
